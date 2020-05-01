379823d4c9c5fbe1306c60c8fc0b1b273afcef58
gagafuture <15473481746@qq.com>
2020-05-01 20:20:04 +0800

    test

--- src/main/java/com/hzp/servlet/TestServlet.java
+++ src/main/java/com/hzp/servlet/TestServlet.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("/TEST")
+@WebServlet("/")
 public class TestServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.getWriter().append("good111");
