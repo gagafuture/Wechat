e989bd737c31d151ed2973747aa5d663e026cc48
gagafuture <15473481746@qq.com>
2020-05-28 22:42:52 +0800

    test

--- src/main/java/com/hzp/Utils/NFDFlightDataTimerTask.java
+++ src/main/java/com/hzp/Utils/NFDFlightDataTimerTask.java
@@ -1,6 +1,8 @@
 package com.hzp.Utils;
 
 import com.hzp.service.TaskSrevice;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -12,16 +14,20 @@ import java.util.TimerTask;
  * @time: 2020/5/7 23:44
  */
 public class NFDFlightDataTimerTask extends TimerTask {
+    public static Log log = LogFactory.getLog(NFDFlightDataTimerTask.class);
     @Override
     public void run() {
         try {
+            log.info("开始执行");
             System.out.println("-------------开始执行--------------");
             //执行操作
             ApplicationContext context  =new ClassPathXmlApplicationContext("applicationContext.xml");
             TaskSrevice taskSrevice = context.getBean(TaskSrevice.class);
             taskSrevice.updateChildrenType();
             System.out.println("-------------执行结束--------------");
+            log.info("执行结束");
         } catch (Exception e) {
+            log.info("解析信息发生异常");
             System.out.println("-------------解析信息发生异常--------------");
         }
     }
--- src/main/java/com/hzp/Utils/TimerManager.java
+++ src/main/java/com/hzp/Utils/TimerManager.java
@@ -1,5 +1,8 @@
 package com.hzp.Utils;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Timer;
@@ -11,6 +14,7 @@ import java.util.Timer;
  */
 public class TimerManager {
     private static final long PERIOD_DAY = 24 * 60 * 60 * 1000;
+    private static Log log = LogFactory.getLog(TimerManager.class);
     public TimerManager() {
         Calendar calendar = Calendar.getInstance();
 
@@ -22,6 +26,7 @@ public class TimerManager {
 
         Date date=calendar.getTime(); //第一次执行定时任务的时间
         System.out.println(date);
+        log.info("当前时间："+date);
         System.out.println("before 方法比较："+date.before(new Date()));
         //如果第一次执行定时任务的时间 小于 当前的时间
         //此时要在 第一次执行定时任务的时间 加一天，以便此任务在下个时间点执行。如果不加一天，任务会立即执行。循环执行的周期则以当前时间为准
@@ -29,7 +34,7 @@ public class TimerManager {
             date = this.addDay(date, 1);
             System.out.println(date);
         }
-
+        log.info("执行时间："+date);
         Timer timer = new Timer();
 
         NFDFlightDataTimerTask task = new NFDFlightDataTimerTask();
